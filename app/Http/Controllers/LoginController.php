18c05d37701e60cae2252f165b8a909673883b84
wangjian <583327347@qq.com>
2018-08-02 21:52:13 +0800

    add

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Http\Controllers;
-
+use DB;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 class LoginController extends Controller{
     function admin(){
         return view('login.login') ;
