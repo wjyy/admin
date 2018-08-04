f5b028406ed910e0dd0a0d2d15bf2d0db90b092f
wangjian <583327347@qq.com>
2018-08-04 15:15:15 +0800

    2

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -13,7 +13,7 @@
 @include('public.header')
 <div id="dcMain"> <!-- 当前位置 -->
     <div id="urHere">DouPHP 管理中心</div>  <div id="index" class="mainBox" style="padding-top:18px;height:auto!important;height:550px;min-height:550px;">
-        <div class="warning">您还没有删除 install 文件夹，出于安全的考虑，我们建议您删除 install 文件夹。</div>
+
 
         <div id="douApi"></div>
         <div class="indexBox">
@@ -42,114 +42,36 @@
                         <ul>
                             <table width="100%" border="0" cellspacing="0" cellpadding="7" class="tableBasic">
                                 <tr>
-                                    <td width="120">单页面数：</td>
-                                    <td><strong>6</strong></td>
-                                    <td width="100">文章总数：</td>
-                                    <td><strong>10</strong></td>
+                                    <td width="120">网站开发人数：</td>
+                                    <td><strong>3</strong></td>
+                                    <td width="100">网站模板</td>
+                                    <td><strong> DouPHP</strong></td>
                                 </tr>
                                 <tr>
-                                    <td>商品总数：</td>
-                                    <td><strong>15</strong></td>
-                                    <td>系统语言：</td>
-                                    <td><strong>zh_cn</strong></td>
+                                    <td width="120">网站开发人员：</td>
+                                    <td><strong>郑爽</strong></td>
+                                    <td><strong>李涛</strong></td>
+                                    <td><strong> 王剑</strong></td>
                                 </tr>
                                 <tr>
-                                    <td>URL 重写：</td>
-                                    <td><strong>关闭<a href="system.php" class="cueRed ml">（点击开启）</a>
-                                        </strong></td>
-                                    <td>编码：</td>
-                                    <td><strong>UTF-8</strong></td>
+                                    <td width="120">网站测试版本：</td>
+                                    <td><strong>1.0版本</strong></td>
+                                    <td width="100">更高版本</td>
+                                    <td><strong> 敬请期待</strong></td>
                                 </tr>
-                                <tr>
-                                    <td>站点地图：</td>
-                                    <td><strong>开启</strong></td>
-                                    <td>站点模板：</td>
-                                    <td><strong>default</strong></td>
-                                </tr>
-                                <tr>
-                                    <td>DouPHP版本：</td>
-                                    <td><strong>v1.3 Release 20160125</strong></td>
-                                    <td>安装日期：</td>
-                                    <td><strong>2016-02-25</strong></td>
-                                </tr>
+
                             </table>
                         </ul>
                     </div>
-                </td>
-                <td valign="top" class="pl">
-                    <div class="indexBox">
-                        <div class="boxTitle">管理员  登录记录</div>
-                        <ul>
-                            <table width="100%" border="0" cellspacing="0" cellpadding="7" class="tableBasic">
-                                <tr>
-                                    <th width="45%">IP地址</th>
-                                    <th width="55%">操作时间</th>
-                                </tr>
-                                <tr>
-                                    <td align="center">127.0.0.1</td>
-                                    <td align="center">2016-02-25 23:29:08</td>
-                                </tr>
-                                <tr>
-                                    <td align="center">127.0.0.1</td>
-                                    <td align="center">2016-02-25 13:48:48</td>
-                                </tr>
-                                <tr>
-                                    <td align="center">127.0.0.1</td>
-                                    <td align="center">2013-10-16 09:43:01</td>
-                                </tr>
-                                <tr>
-                                    <td align="center">127.0.0.1</td>
-                                    <td align="center">2013-10-16 09:42:58</td>
-                                </tr>
-                            </table>
-                        </ul>
-                    </div>
-                </td>
-            </tr>
-        </table>
-        <div class="indexBox">
-            <div class="boxTitle">服务器信息</div>
-            <ul>
-                <table width="100%" border="0" cellspacing="0" cellpadding="7" class="tableBasic">
-                    <tr>
-                        <td width="120" valign="top">PHP 版本：</td>
-                        <td valign="top">5.3.29 </td>
-                        <td width="100" valign="top">MySQL 版本：</td>
-                        <td valign="top">5.5.40</td>
-                        <td width="100" valign="top">服务器操作系统：</td>
-                        <td valign="top">WINNT(127.0.0.1)</td>
-                    </tr>
-                    <tr>
-                        <td valign="top">文件上传限制：</td>
-                        <td valign="top">2M</td>
-                        <td valign="top">GD 库支持：</td>
-                        <td valign="top">是</td>
-                        <td valign="top">Web 服务器：</td>
-                        <td valign="top">Apache/2.4.10 (Win32) OpenSSL/0.9.8zb mod_fcgid/2.3.9</td>
-                    </tr>
-                </table>
-            </ul>
-        </div>
-        <div class="indexBox">
-            <div class="boxTitle">系统开发</div>
-            <ul>
-                <table width="100%" border="0" cellspacing="0" cellpadding="7" class="tableBasic">
-                    <tr>
-                        <td width="120"> DouPHP官网： </td>
-                        <td><a href="http://www.douco.com" target="_blank">http://www.douco.com</a></td>
-                    </tr>
-                    <tr>
-                        <td> 开发者社区： </td>
-                        <td><a href="http://bbs.douco.cn" target="_blank">http://bbs.douco.cn </a><em>（安装使用 模板交流 BUG反馈 意见建议）</em></td>
-                    </tr>
-                    <tr>
-                        <td> 贡献者： </td>
-                        <td>Wooyun.org, Pany, Tea</td>
-                    </tr>
-                    <tr>
-                        <td> 系统使用协议： </td>
-                        <td><a href="http://www.douco.com/license.html" target="_blank">http://www.douco.com/license.html</a><em>（您可以免费使用DouPHP（不限商用），但必须保留相关版权信息。）</em></td>
-                    </tr>
+
+                        <div><p >
+
+                                <h1 align="center">北京鸿博互联软件有限公司</h1> <br>
+
+
+                                北京鸿博互联软件有限公司于2016年04月08日成立。法定代表人王成,公司经营范围包括：软件开发；技术开发、技术推广、技术转让、技术咨询、技术服务；计算机系统服务；软件咨询；产品设计；模型设计；教育咨询(中介服务除外）；企业策划；企业管理咨询；组织文化艺术交流活动（不含营业性演出）；承办展览展示活动；会议服务；影视策划；数据处理(数据处理中的银行卡中心、PUE值在1.5以上的云计算数据中心除外)；销售自行开发后的产品；计算机技术培训（不得面向全国招生）等。
+
+                            </p></div>
                 </table>
             </ul>
         </div>
